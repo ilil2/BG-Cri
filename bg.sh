914c3ab0acb67348f71b7a854b3d200031e5acae
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2025-09-23 13:19:47 +0200

    Init

---
+++ bg.sh
@@ -0,0 +1,12 @@
+if [ "$1" == "" ]; then
+	me=$(whoami)
+else
+	me=$1
+fi
+link="https://cri.epita.fr/photos/thumb/$me"
+cookie=$(curl -i -s -L https://cri.epita.fr/users/ | grep -i Set-Cookie | awk -F';' '{print $1}')
+crfs=$($cookie | grep "crfs_token")
+op=$($cookie | grep "op_browser_state" | head -n 1)
+id=$(nix-shell -p sqlite --command "sqlite3 cookies.sqlite \"SELECT value FROM moz_cookies WHERE host='cri.epita.fr' AND name='sessionid'\"" | tail -n 1)
+
+curl -b "crfs_token=$crfs; op_browser_state=$op; sessionid=$id" -L $link | feh --bg-scale -
