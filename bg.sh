c8b86ccce2be9ee43378b3c27a2d3daf5504a6d5
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2025-09-23 16:33:11 +0200

    It work

--- bg.sh
+++ bg.sh
@@ -4,9 +4,11 @@ else
 	me=$1
 fi
 link="https://cri.epita.fr/photos/thumb/$me"
-cookie=$(curl -i -s -L https://cri.epita.fr/users/ | grep -i Set-Cookie | awk -F';' '{print $1}')
-crfs=$($cookie | grep "crfs_token")
-op=$($cookie | grep "op_browser_state" | head -n 1)
-ids=$(nix-shell -p sqlite --command "sqlite3 ~/.mozilla/firefox/*.default/cookies.sqlite \"SELECT value FROM moz_cookies WHERE host='cri.epita.fr' AND name='sessionid'\"" | tail -n 1)
+cookie=$(curl -i -s -L https://cri.epita.fr/users/ --negotiate | grep -i Set-Cookie | awk -F';' '{print $1}')
+crfs=$(echo "$cookie" | grep "crfstoken" | head -n 1 | cut -d'=' -f2)
+op=$(echo "$cookie" | grep "op_browser_state" | head -n 1 | cut -d'=' -f2)
+ids=$(echo "$cookie" | grep "sessionid" | head -n 1 | cut -d'=' -f2)
+
+echo "$crfs $op $ids"
 
 curl -b "crfs_token=$crfs; op_browser_state=$op; sessionid=$ids" -L $link | feh --bg-scale -
