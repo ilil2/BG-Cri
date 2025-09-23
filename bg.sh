a70550b6df388d4274d7c60d3331d8fa3818c8dd
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2025-09-23 16:34:33 +0200

    remove echo

--- bg.sh
+++ bg.sh
@@ -9,6 +9,4 @@ crfs=$(echo "$cookie" | grep "crfstoken" | head -n 1 | cut -d'=' -f2)
 op=$(echo "$cookie" | grep "op_browser_state" | head -n 1 | cut -d'=' -f2)
 ids=$(echo "$cookie" | grep "sessionid" | head -n 1 | cut -d'=' -f2)
 
-echo "$crfs $op $ids"
-
 curl -b "crfs_token=$crfs; op_browser_state=$op; sessionid=$ids" -L $link | feh --bg-scale -
